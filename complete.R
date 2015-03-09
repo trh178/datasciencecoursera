efd906a96e137509c66b1ee9318748eba2e0c20f
Todd Haldeman <thaldeman@listrak.com>
2015-03-09 16:59:51 -0400

    finished problem 2 on assignment 1

--- complete.R
+++ complete.R
@@ -1,3 +1,34 @@
 complete <- function(directory, id=1:332) {
-    
+    path <- file.path(directory, sprintf("%03d", id))
+    nobs <- numeric()
+    for(p in path) {
+        data <- read.csv(paste(p, ".csv", sep=""))
+        good.data <- na.omit(data)
+        nobs <- c(nobs, nrow(good.data))
+    }
+    data.frame(id, nobs)
 }
+
+# complete("specdata", 1)
+# ##   id nobs
+# ## 1  1  117
+# complete("specdata", c(2, 4, 8, 10, 12))
+# ##   id nobs
+# ## 1  2 1041
+# ## 2  4  474
+# ## 3  8  192
+# ## 4 10  148
+# ## 5 12   96
+# complete("specdata", 30:25)
+# ##   id nobs
+# ## 1 30  932
+# ## 2 29  711
+# ## 3 28  475
+# ## 4 27  338
+# ## 5 26  586
+# ## 6 25  463
+# complete("specdata", 3)
+# ##   id nobs
+# ## 1  3  243
+
+
