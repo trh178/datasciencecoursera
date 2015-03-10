094cf094eec861e91af50348ade05100a3adc651
Todd Haldeman <thaldeman@listrak.com>
2015-03-10 13:45:33 -0400

    finished assignment 1

--- corr.R
+++ corr.R
@@ -1,3 +1,41 @@
 corr <- function(directory, threshold=0) {
+    df.complete <- complete(directory)
+    df.good <- df.complete[df.complete$nobs > threshold,]
     
+    corr.vect <- numeric()
+    for(id in df.good$id) {
+        path <- paste(file.path(directory, sprintf("%03d", id)), ".csv", sep="")
+        data <- read.csv(path)
+        data.nona <- na.omit(data)
+        val <- cor(data.nona$nitrate, data.nona$sulfate)
+        corr.vect <- c(corr.vect, val)
+    }
+    corr.vect
 }
+
+source("complete.R")
+cr <- corr("specdata", 150)
+head(cr)
+## [1] -0.01896 -0.14051 -0.04390 -0.06816 -0.12351 -0.07589
+summary(cr)
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+## -0.2110 -0.0500  0.0946  0.1250  0.2680  0.7630
+cr <- corr("specdata", 400)
+head(cr)
+## [1] -0.01896 -0.04390 -0.06816 -0.07589  0.76313 -0.15783
+summary(cr)
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+## -0.1760 -0.0311  0.1000  0.1400  0.2680  0.7630
+cr <- corr("specdata", 5000)
+summary(cr)
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+## 
+length(cr)
+## [1] 0
+cr <- corr("specdata")
+summary(cr)
+##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+## -1.0000 -0.0528  0.1070  0.1370  0.2780  1.0000
+length(cr)
+## [1] 323
+
