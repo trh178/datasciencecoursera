4d15ac8f0a526e389f3ac29cafc9cda328d1b63e
Todd Haldeman <thaldeman@listrak.com>
2015-03-12 11:08:00 -0400

    stuff needed for quiz 2

---
+++ course-2-quiz-2.R
@@ -0,0 +1,25 @@
+cube <- function(x,n) {
+    x^3
+}
+
+x <- 1:10
+if(x > 5) {
+    x <- 0
+}
+
+f <- function(x) {
+    g <- function(y) {
+        y + z
+    }
+    z <- 4
+    x + g(x)
+}
+
+x <- 5
+y <- if(x < 3) {
+    NA
+} else {
+    10
+}
+
+ 
