f5372ab3866515456ac68053ba10354eb9023e2f
Todd Haldeman <thaldeman@listrak.com>
2015-05-22 11:18:14 -0400

    finished investigation on pt 1

--- SI-proj.R
+++ SI-proj.R
@@ -5,19 +5,31 @@ for (i in 1 : 1000) mns = c(mns, mean(runif(40)))
 hist(mns)
 
 #project
+n <- 1000
 lambda <- 0.2
 mean.theoretical <- 1/lambda
 sd.theoretical <- 1/lambda
+var.theoretical <- 1/lambda^2
 
-v1 <- rexp(1000, lambda)
+v1 <- rexp(n, lambda)
 hist(v1)
 
 v2 <- NULL
-for(i in 1:1000) v2 <- c(v2, mean(rexp(40,lambda)))
+se <- sqrt(var.theoretical / n)
+cfunc <- function(x,n) (x - mean.theoretical) / se
+for(i in 1:n) v2 <- c(v2, mean(rexp(40,lambda)))
+v3 <- sapply(v2, cfunc)
 hist(v2)
+hist(v3)
 
+# pt1
 mean.theoretical
 mean.sample <- mean(v2)
 
-sd.sample <- sd(mns.exp)
-sd.ideal
+# pt2
+var.theoretical
+var(v3)
+
+# pt3
+hist(v1)
+hist(v2)
